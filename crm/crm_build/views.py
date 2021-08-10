724a4c333357eb3fa813abfa010fa984b5e50d3f
Chaplygin-vlad <metro_flava@mail.ru>
2021-08-10 17:26:07 +0300

    create pagination draft

--- crm/crm_build/views.py
+++ crm/crm_build/views.py
@@ -11,7 +11,13 @@ def index(request):
 class MainPageListView(ListView):
     """Главная страница, выводит сразу список всех объектов"""
     model = RlClient
+    context_object_name = 'objects'
+    template_name = 'index.html'
+    paginate_by = 10
+
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        # context['objects'] = context['objects'].filter(lot=381)
+
         return context
