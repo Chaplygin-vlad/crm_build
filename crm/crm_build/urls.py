caa81a98a727d5e4b32d031f189915c7269e18f0
Vladislav <57090437+Chaplygin-vlad@users.noreply.github.com>
2021-08-12 14:43:36 +0300

    Feature/create all objects view (#8)
    * delete empty tables from models.py
    
    * comment columns and delete empty
    
    * complete all clients draft page
    
    * complete all realty objects draft page
    
    * add urls
    
    Co-authored-by: Akinshin Anatolii <a.akinshin@satvaspace.com>

--- crm/crm_build/urls.py
+++ crm/crm_build/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from crm_build.views import MainPageListView, index
+from crm_build.views import MainPageListView, AllSaleObjectListView
 
 urlpatterns = [
     path('', MainPageListView.as_view(), name='index'),
+    path('all_sale/', AllSaleObjectListView.as_view(), name='all_sale')
+
 ]
--- crm/crm_build/views.py
+++ crm/crm_build/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Subquery, OuterRef
+from django.db.models import Subquery, OuterRef, F, ExpressionWrapper, IntegerField
 from django.shortcuts import render
 from django.views.generic import ListView
 
@@ -18,6 +18,33 @@ class MainPageListView(ListView):
 
     def get_queryset(self):
         queryset = RlClient.objects.exclude(
+            status_enum='Архив (без сделки)'
+        ).annotate(
+            agent=Subquery(
+                SysUser.objects.filter(
+                    row_id=OuterRef("user_id")).values("name")
+            ),
+            last_agent=Subquery(
+                SysUser.objects.filter(
+                    row_id=OuterRef("last_work_user_id")).values("name")
+            )
+        )
+
+        return queryset
+
+
+class AllSaleObjectListView(ListView):
+    """Выводит все объекты недвижимости"""
+    model = RlClient
+    context_object_name = 'objects'
+    template_name = 'all_sale_objects.html'
+    paginate_by = 10
+    expression = F('cost')/F('area1')
+    wrapped_expression = ExpressionWrapper(expression, IntegerField())
+    queryset = RlClient.objects.all().annotate(square_price=wrapped_expression)
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(client_enum='Продажа').exclude(
             status_enum='Архив (без сделки)'
         ).annotate(
             agent=Subquery(
