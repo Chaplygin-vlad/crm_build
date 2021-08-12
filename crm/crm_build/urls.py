3ec4d4337c9c6b889638b95e701fbbfaa25ceba8
Chaplygin-vlad <metro_flava@mail.ru>
2021-08-12 12:10:29 +0300

    add urls

--- crm/crm_build/urls.py
+++ crm/crm_build/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from crm_build.views import MainPageListView, index
+from crm_build.views import MainPageListView, AllSaleObjectListView
 
 urlpatterns = [
     path('', MainPageListView.as_view(), name='index'),
+    path('all_sale/', AllSaleObjectListView.as_view(), name='all_sale')
 
     # path('', MainPageListView.as_view(), name='base'),
 
