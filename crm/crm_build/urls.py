f981cabde8e01ecd3e66b97996ad5fb903885470
Vladislav <57090437+Chaplygin-vlad@users.noreply.github.com>
2021-08-18 17:31:16 +0300

    Feature/create buyer detail view (#20)
    * delete empty tables from models.py
    
    * comment columns and delete empty
    
    * create urls and view
    
    * work for template
    
    * fix 404 problem
    
    * commit
    
    * add sell object params
    
    * fix endif
    
    * complete object card
    
    Co-authored-by: Akinshin Anatolii <a.akinshin@satvaspace.com>

--- crm/crm_build/urls.py
+++ crm/crm_build/urls.py
@@ -4,6 +4,7 @@ from crm_build.views import (
     MainPageListView,
     AllSaleObjectListView,
     AllBuyersListView,
+    BuyerDetail,
     PhotosView
 )
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', MainPageListView.as_view(), name='index'),
     path('all_sale/', AllSaleObjectListView.as_view(), name='all_sale'),
     path('all_buyers/', AllBuyersListView.as_view(), name='all_buyers'),
-    path('<int:obj_id>/photos/', PhotosView.as_view(), name='photos')
+    path('<int:obj_id>/photos/', PhotosView.as_view(), name='photos'),
+    path('<int:row_id>/object/', BuyerDetail.as_view(), name='object')
 
 ]
