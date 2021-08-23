ea098224de7700eb6f6469139f83a3acb19ef764
Anatoly <kairickaav@gmail.com>
2021-08-23 16:43:19 +0300

    add deploy (#30)
    Co-authored-by: Akinshin Anatolii <a.akinshin@satvaspace.com>

---
+++ docker_update.py
@@ -0,0 +1,13 @@
+import sys
+
+secret = str(sys.argv[1])
+host = str(sys.argv[2])
+user = str(sys.argv[3])
+passwd = str(sys.argv[4])
+with open('Dockerfile', 'a') as docker:
+    docker.write(f'ENV SECRET_KEY={secret} \n')
+    docker.write(f'ENV DB_NAME={host} \n')
+    docker.write(f'ENV DB_USER={user} \n')
+    docker.write(f'ENV DB_PASSWORD={passwd} \n')
+    docker.write(f'RUN python manage.py collectstatic --no-input \n')
+    docker.write(f'RUN chmod -R 775 static')
